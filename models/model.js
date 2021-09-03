49203cd4259f66fa6a4bc859040b265f6071451c
Nisha Yadav <nisha.yadav@growthnatives.com>
2021-09-03 23:12:55 +0530

    second commit

--- models/model.js
+++ models/model.js
@@ -2,19 +2,24 @@ const mongoose            = require('mongoose');
 const Schema              = mongoose.Schema;
 
 const dataSchema = new Schema({
-        name: String,
-        minimum: Number,
-        delFee: Number,
-        Location : {
+    name: String,
+    minimum: Number,
+    delFee: Number,
+    location: {
+        type: {
             type: String,
-            Coordinates: Number
-            },
-        status: { type: Boolean, default: true }
+            enum: ["Point", "Polygon"]
+        },
+        coordinates: [{ type: Number }], // long, lat
+    },
+    status: { type: Boolean, default: true }
 },
     {
         timestamps: true,
         toJSON: { virtuals: true },
         toObject: { virtuals: true }
     })
+
+dataSchema.index({ location: "2dsphere" });
 
 module.exports = mongoose.model("problem", dataSchema);
--- routes/model.js
+++ routes/model.js
@@ -2,94 +2,81 @@ const express = require('express')
 const router = express.Router()
 const problem = require('../models/model')
 
-router.get('/list', async(req,res) => {
-    try{
-           const aliens = await problem.find()
-           res.json(aliens)
-    }catch(err){
+router.get('/list', async (req, res) => {
+    try {
+        const aliens = await problem.find()
+        res.json(aliens)
+    } catch (err) {
         res.send('Error ' + err)
     }
 })
 
 
-router.post('/create', async(req,res) => {
+router.post('/create', async (req, res) => {
+    let location = {
+        type: "Point",
+        coordinates: [req.body.lng, req.body.lat],
+    }
     const alien = new problem({
         name: req.body.name,
-        location: {
-            type :req.body.type,
-            coordinates:[
-                req.body.lat,
-                 req.body.long, 
-            ]
-        },
+        location: location,
         minimum: req.body.minimum,
         delFee: req.body.delFee,
         status: req.body.status
     })
 
-    try{
-        const a1 =  await alien.save() 
+    try {
+        const a1 = await alien.save()
         console.log(a1);
         res.json(a1)
-    }catch(err){
-        console.log(error)
+    } catch (err) {
+        console.log(err)
         res.send('Error')
     }
 })
 
-router.put('/update/:id',async(req,res)=> {
-    try{
-        const app = await problem.findById(req.params.id) 
+router.put('/update/:id', async (req, res) => {
+    try {
+        const app = await problem.findById(req.params.id)
         app.name = req.body.name,
-        app.minimum= req.body.minimum,
-       app.delFee= req.body.delFee,
-        app.status= req.body.status,
-        app.location= {
-            type :req.body.type,
-            coordinates:[
-                req.body.lat,
-                 req.body.long, 
-            ]
-        }
+            app.minimum = req.body.minimum,
+            app.delFee = req.body.delFee,
+            app.status = req.body.status,
+            app.location = {
+                type: req.body.type,
+                coordinates: [
+                    req.body.lat,
+                    req.body.long,
+                ]
+            }
         const a1 = await app.save()
-        res.json(a1)   
-    }catch(err){
+        res.json(a1)
+    } catch (err) {
         res.send('Error')
     }
 
 })
 
-router.post('/checklocation', async(req,res) => {
-    const alien = new problem({
-        name: req.body.name,
-        location: {
-            coordinates:[
-                req.body.lat,
-                 req.body.long, 
-            ]
-        }
-    })
-
-    try{
-        const a1 =  await alien.save() 
-        Distance = await problem.find(
+router.post('/checklocation', async (req, res) => {
+    let { lng, lat } = req.body;
+    try {
+        await problem.aggregate([
             {
-             location :{
-              $near:
-              {
-                $geometry: {
-                  type: "Point",
-                  coordinates: [req.body.long, req.body.lat]
-                },
-             $maxDistance:1000*1000
+                $geoNear: {
+                    near: { type: "Point", coordinates: [lng, lat] },
+                    distanceField: "dist.calculated",
+                    minDistance: 20000,
+                    includeLocs: "dist.location",
+                    spherical: true
+                }
             }
-          }
-         }
-         );
-        console.log(a1);
-        res.json(Distance);
-    }catch(err){
-        console.log(error)
+        ]).exec((err, results) => {
+            console.log(results,err);
+            res.json(results);
+        });
+
+    } catch (err) {
+        console.log(err)
         res.send('Error')
     }
 })
